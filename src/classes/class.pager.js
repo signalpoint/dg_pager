d1ebc0d2e608dbd7d223e6f8cb57f120f2ae2652
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-03-26 23:18:13 -0400

    Add _infiniteOps and pagesAllowed to support intelligent DOM cleanup.

--- src/classes/class.pager.js
+++ src/classes/class.pager.js
@@ -2,9 +2,11 @@
  *
  * @param id
  * @param variables
- *  'fetcher'
- *  'display'
- *  'infinite'
+ *  '_fetcher'
+ *  '_display'
+ *  '_infinite'
+ *  '_infiniteOps' {Object}
+ *      pagesAllowed {Number}
  */
 var Pager = function(id, variables) {
 
@@ -21,6 +23,11 @@ var Pager = function(id, variables) {
     limit: variables._limit ? variables._limit : 10
   };
 
+  // Set up default options for infinite scroll if it is configured for infinite.
+  if (variables._infinite && !variables._infiniteOps) {
+    variables._infiniteOps = { pagesAllowed: 2 };
+  }
+
   // Add this pager to the globals for reference.
   dg._pagers[this.id()] = this;
 
@@ -29,6 +36,7 @@ var Pager = function(id, variables) {
 Pager.prototype.id = function() { return this._id; };
 Pager.prototype.getVars = function() { return this._variables; };
 Pager.prototype.getVar = function(name) { return this.getVars()['_' + name]; };
+Pager.prototype.getOption = function(name) { return this.getVars()._infiniteOps[name]; };
 
 Pager.prototype.getFetcher = function() { return this.getVar('fetcher'); };
 
@@ -43,6 +51,10 @@ Pager.prototype.runDone = function() {
   var done = this.getVar('done');
   if (done) { done.call(this); }
 };
+
+Pager.prototype.getList = function() { return dg.qs('#' + this.getWrapperId()); };
+Pager.prototype.getItems = function() { return this.getList().children; };
+Pager.prototype.getItem = function(index) { return this.getItems()[index]; };
 
 Pager.prototype.setData = function(data) { this._data = data; };
 Pager.prototype.setPage = function(page) { this.getData().page = page; };
@@ -82,6 +94,7 @@ Pager.prototype.fetch = function() {
     });
   });
 };
+
 Pager.prototype.render = function() {
   var self = this;
   return new Promise(function(ok, err) {
@@ -166,6 +179,38 @@ Pager.prototype.renderPager = function() {
 
 Pager.prototype.isInfinite = function() { return this.getVar('infinite'); };
 Pager.prototype.eventsAdded = function() { return this._eventsAdded; };
+Pager.prototype.getDirection = function() { return this._direction; };
+Pager.prototype.setDirection = function(direction) { this._direction = direction; };
+
+Pager.prototype.getRowCountInDom = function() {
+  return this.getList().childElementCount;
+};
+Pager.prototype.getPagesInDom = function() {
+  return Math.ceil(this.getRowCountInDom() / this.getLimit());
+};
+Pager.prototype.removeFromTop = function() {
+  //console.log('removing from top');
+  var list = this.getList();
+  var stop = this.getLimit();
+  //console.log('removing this many items', stop);
+  for (var i = 0; i < stop; i++) {
+    var child = list.childNodes[0];
+    //console.log('removing', 0, i, child);
+    list.removeChild(child);
+  }
+};
+Pager.prototype.removeFromBottom = function() {
+  //console.log('removing from bottom');
+  var list = this.getList();
+  var start = list.childNodes.length;
+  var stop = start - this.getLimit();
+  //console.log('removing from/to', start, stop);
+  for (var i = start - 1; i >= stop; i--) {
+    var child = list.childNodes[i];
+    //console.log('removing', i, child);
+    list.removeChild(child);
+  }
+};
 
 Pager.prototype.addEvents = function() {
   //console.log(this.id(), 'adding events');
@@ -177,11 +222,13 @@ Pager.prototype.addEvents = function() {
     var eventListeners = dg_pager.infiniteScrollEventListeners();
     eventListeners.forEach(function(name) {
       addEventListener(name, self.onVisibilityChangeHandler, { passive: true });
+      addEventListener(name, self.firstItemOnVisibilityChangeHandler, { passive: true });
     });
   } else if (window.attachEvent)  { // IE9+ :(
     var events = dg_pager.infiniteScrollEvents();
     events.forEach(function(name) {
       attachEvent(name, self.onVisibilityChangeHandler, false);
+      attachEvent(name, self.firstItemOnVisibilityChangeHandler, false);
     });
   }
   self._eventsAdded = true;
@@ -197,11 +244,13 @@ Pager.prototype.removeEvents = function() {
     var eventListeners = dg_pager.infiniteScrollEventListeners();
     for (var i = 0; i < eventListeners.length; i++) {
       removeEventListener(eventListeners[i], self.onVisibilityChangeHandler);
+      removeEventListener(eventListeners[i], self.firstItemOnVisibilityChangeHandler);
     }
   } else if (window.removeEvent)  { // IE9+ :(
     var events = dg_pager.infiniteScrollEvents();
     for (var j = 0; j < events.length; j++) {
       removeEvent(events[j], self.onVisibilityChangeHandler);
+      removeEvent(events[j], self.firstItemOnVisibilityChangeHandler);
     }
   }
   self._eventsAdded = false;
@@ -209,67 +258,173 @@ Pager.prototype.removeEvents = function() {
 
 Pager.prototype.toInfinityAndBeyond = function() {
   var self = this;
-  var theme = this.getWrapperTheme();
 
-  // Grab the last item from data results.
+  // By this point every row is rendered in the DOM's list and visible to the user...
+
+  // Let's grab the list, or bail out if we can't find it.
   var list = document.getElementById(this.getVar('wrapperId'));
-  var el = list ? list.lastChild : null;
+  if (!list) { return; }
 
-  // Set up a handler to watch for the list item to come into view.
-  this.onVisibilityChangeHandler = onVisibilityChange(el, function(visible) {
+  // Grab the first item in the list.
+  var first = list.firstChild;
+
+  this.firstItemOnVisibilityChangeHandler = onVisibilityChange(first, function(visible) {
 
     if (visible) {
-      //console.log('got to bottom!!', self.id());
+      //console.log('got to top!!', self.id());
+
+      // If we're at the top of the first page, just skip the fact that the first item has come back into the view port.
+      if (self.getPage() === 0) {
+        //console.log('was at top of page zero, do nothing...');
+        return;
+      }
 
       self.removeEvents();
-
-      var which = 'next';
-      var currentPage = self.getPage();
-      var destinationPage = which == 'next' ? currentPage + 1 : currentPage - 1;
-      if (destinationPage < 0) { return; } // reached the first page.
-      else if (destinationPage >= self.getPages()) { return; } // Reached the last page.
-      self.setPage(destinationPage);
-      self.fetch().then(function() {
-
-        // @TODO need to update the first/last classes here.
-        var rows = self.renderRows();
-        var html = '';
-        for (var i = 0; i < rows.length; i++) {
-          var item = rows[i];
-          switch (theme) {
-
-            case 'bootstrap_item_list':
-                dg_bootstrap.prepListItem(item);
-                html += dg.render({
-                  _theme: 'list_item',
-                  _item: item,
-                  _i: (i * destinationPage) + i,
-                  _total: rows.length
-                });
-              break;
-
-            case 'container':
-                html += dg.render(item);
-              break;
-
-          }
-
-        }
-        dg.qs('#' + self.getWrapperId()).innerHTML += html;
-
-        self.runDone();
-        self.toInfinityAndBeyond();
-
-      });
+      self._toInfinityAndBeyond('up');
 
     }
 
   });
 
+  // Grab the last item in the list.
+  var el = list.lastChild;
+
+  // Set up a handler to watch for the last list item to come into view.
+  this.onVisibilityChangeHandler = onVisibilityChange(el, function(visible) {
+
+    if (visible) {
+      //console.log('got to bottom!!', self.id());
+
+      // @TODO catch when we're on the very last page? similar to what we do for the first page above?
+      // If we're at the bottom of the very last page, just skip the fact that the last item has come back into the view
+      // port.
+      if (self.getPage() + 1 == self.getPages()) {
+        //console.log('was at bottom of the last page, do nothing...');
+        return;
+      }
+
+      self.removeEvents();
+      self._toInfinityAndBeyond('down');
+    }
+
+  });
+
   self.addEvents();
+  //console.log('----------------------');
+
+};
+
+Pager.prototype._toInfinityAndBeyond = function(direction) {
+  var self = this;
+
+  // Did we switch directions?
+  var lastDirection = this.getDirection();
+  var switchedDirections = lastDirection && lastDirection != direction;
+  this.setDirection(direction);
+
+  // Figure out what direction we're going.
+  // What direction are we going?
+  var goingDown = direction == 'down';
+
+  // Figure out what page we're on, and what page we're going to.
+  var currentPage = this.getPage();
+  var pagesAllowed = this.getOption('pagesAllowed');
+  var destinationPage = null;
+  if (goingDown) {
+    destinationPage = switchedDirections ? currentPage + pagesAllowed : currentPage + 1;
+  }
+  else {
+    destinationPage = switchedDirections ? currentPage - pagesAllowed : currentPage - 1;
+  }
+  //console.log('----- (switched: ' + switchedDirections + ') ' + currentPage + ' => ' + destinationPage + ' -----');
+
+  // Stop if we reached the end.
+  if (
+      destinationPage < 0 || // Reached the first page.
+      destinationPage >= this.getPages() // Reached the last page.
+  ) { return; }
+
+
+  // Set up some helper vars.
+  var theme = this.getWrapperTheme();
+  var pagesInDom = this.getPagesInDom();
+  //console.log('currentPage', currentPage);
+  //console.log('destinationPage', destinationPage);
+  //console.log('pagesInDom', pagesInDom);
+
+  // If there are too many pages in the DOM, remove the oldest one (depending on what direction we're moving).
+  if (pagesInDom >= pagesAllowed) {
+    //console.log('too many pages in the DOM...');
+    var scrollTo = null;
+    if (goingDown) {
+      this.removeFromTop();
+      scrollTo = this.getLimit() * (pagesAllowed - 1) - 1;
+      //console.log('scrolling down to', scrollTo);
+      this.getItem(scrollTo).scrollIntoView({block: "end", inline: "start"});
+    }
+    else {
+      this.removeFromBottom();
+      scrollTo = this.getLimit() - 1;
+    }
+  }
+
+  // Set the new page and fetch the data.
+  this.setPage(destinationPage);
+  this.fetch().then(function() {
+
+    // @TODO need to update the first/last classes here.
+
+    // Render all the rows. Note, many developers will return a render element here so the rows wont' technically be
+    // rendered yet in every case, that's why we run them through the render layer below. This allows a row to be
+    // its own render element, for maximum flexibility.
+    var rows = self.renderRows();
+
+    // Now that all of the rows are ready, iterate over each, then render and append them to our result html.
+    var html = '';
+    for (var i = 0; i < rows.length; i++) {
+      var item = rows[i];
+      switch (theme) {
+
+        case 'item_list':
+        case 'bootstrap_item_list':
+          if (theme == 'bootstrap_item_list') { dg_bootstrap.prepListItem(item); }
+          html += dg.render({
+            _theme: 'list_item',
+            _item: item,
+            _i: (i * destinationPage) + i,
+            _total: rows.length
+          });
+          break;
+
+        case 'container':
+          html += dg.render(item);
+          break;
+
+      }
+
+    }
+
+    // Append/prepend the html onto the list depending on which direction we're going.
+    var list = dg.qs('#' + self.getWrapperId());
+    if (goingDown) { list.innerHTML += html; }
+    else {
+      list.insertAdjacentHTML("afterbegin", html);
+      scrollTo = self.getLimit() - 1;
+      //console.log('scrolling up to', scrollTo);
+      self.getItem(scrollTo).scrollIntoView();
+    }
+
+    self.runDone();
+    self.toInfinityAndBeyond();
+
+  });
 
 };
 
 Pager.prototype.onVisibilityChangeHandler = function() {
+  return null; // Empty prototype which is overwritten inside of toInfinityAndBeyond().
+};
+
+Pager.prototype.firstItemOnVisibilityChangeHandler = function() {
   return null; // Empty prototype which is overwritten inside of toInfinityAndBeyond().
 };
