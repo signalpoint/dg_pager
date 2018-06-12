ee54ff7e1eb0ab8d4021a6ee4d72b1692325e5e7
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-06-12 12:26:45 -0400

    Implement and use dg_pager.clearAll() before a route changes to disconnect the first and last observers if they are present.

--- src/includes/include.common.js
+++ src/includes/include.common.js
@@ -2,13 +2,23 @@ dg_pager.load = function(id) {
   return dg._pagers[id] ? dg._pagers[id] : null;
 };
 
-//dg_pager.clearAll = function() {
-//  for (var id in dg._pagers) {
-//    if (!dg._pagers.hasOwnProperty(id)) { continue; }
-//    var pager = dg._pagers[id];
-//    // @TODO remove intersection observers?
-//  }
-//};
+dg_pager.clearAll = function() {
+  for (var id in dg._pagers) {
+    if (!dg._pagers.hasOwnProperty(id)) { continue; }
+    var pager = dg._pagers[id];
+    //console.log(pager);
+
+    // Disconnect the first and last intersection observers, if any.
+    if (pager._firstObserver) {
+      //console.log('disconnecting ' + pager.id() + ' first');
+      pager._firstObserver.disconnect();
+    }
+    if (pager._lastObserver) {
+      //console.log('disconnecting ' + pager.id() + ' last');
+      pager._lastObserver.disconnect();
+    }
+  }
+};
 
 dg_pager.onclickHandler = function(id, which) {
   var pager = dg_pager.load(id);
--- src/includes/include.hooks.js
+++ src/includes/include.hooks.js
@@ -4,6 +4,6 @@
 function dg_pager_pre_process_route_change(newPath, oldPath) {
   if (typeof oldPath !== 'undefined') {
     // @TODO maybe we need to call this when it's implemented?
-    //dg_pager.clearAll();
+    dg_pager.clearAll();
   }
 }
