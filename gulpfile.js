940dff43aac4bb82f6b33a6351040080ecaaa9e1
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2021-08-01 22:04:51 -0400

    Upgrade gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,39 @@
+var makeBinary = true;
+
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     gp_concat = require('gulp-concat'),
     gp_rename = require('gulp-rename'),
-    gp_uglify = require('gulp-uglify');
+    gp_uglify = require('gulp-uglify'),
+    gp_minify_css = require('gulp-minify-css');
 
-var dg_pagerModuleSrc = [
+var jsSrc = [
   './src/_dg_pager.js',
   './src/classes/class.*.js',
-  './src/blocks/blocks.*.js',
-  './src/forms/form.*.js',
   './src/includes/include.*.js',
-  './src/pages/page.*.js',
   './src/widgets/widget.*.js'
 ];
 
-// Task to build the cw_app.min.js file.
-gulp.task('minifyJS', function(){
-  return gulp.src(dg_pagerModuleSrc)
-      .pipe(gp_concat('concat.js'))
-      .pipe(gulp.dest(''))
-      .pipe(gp_rename('dg_pager.min.js'))
-      .pipe(gp_uglify())
-      .pipe(gulp.dest(''));
-});
+// Minify JavaScript
+function minifyJs() {
+  console.log('compressing dg_pager.js...');
+  var js = gulp.src(jsSrc)
+    .pipe(gp_concat('dg_pager.js'))
+    .pipe(gulp.dest('./'));
+  if (makeBinary) {
+    console.log('compressing dg_pager.min.js...');
+    return js.pipe(gp_rename('dg_pager.min.js'))
+    .pipe(gp_uglify())
+    .pipe(gulp.dest('./'));
+  }
+  return js;
+}
+gulp.task(minifyJs);
 
-gulp.task('default', function () {
-  watch(dg_pagerModuleSrc, function(event) { gulp.start('minifyJS') });
-  gulp.start('minifyJS');
+gulp.task('default', function(done) {
+
+  gulp.watch(jsSrc, gulp.series('minifyJs'));
+
+  done();
+
 });
